453320361d14e7d54ba37125a3e5cc2e06da4cac
Phelipe Soares Simeao <pssimeao@stefanini.com>
2020-01-22 13:28:23 -0300

    tipos de parâmetros

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+// Métodos HTTP:  get, post, put, delete
+
+//Tipos de parâmetros: 
+//Query Params: request.query (Filtros, ordenação, paginação, ...)
+//Route Params: request.params (identificar um recurso na alteração ou remoção)
+//Body: request.body (Dados para criação ou alteração de um registro)
+
+//MongoDB (Não-relacional)
+
+app.post('/users', (request, response) => {
+    console.log(request.body);
     return response.json({ message: 'Hello World' });
 });
 
