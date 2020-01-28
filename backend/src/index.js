fcedeb4f144b7065a32ff91039ac145134d6c63c
unknown <authenthi@gmail.com>
2020-01-28 20:24:30 -0300

    terminado front end

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 mongoose.connect('mongodb+srv://omnistack:st1ev2e3@omnistack-dhvng.mongodb.net/week10?retryWrites=true&w=majority', {
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://omnistack:st1ev2e3@omnistack-dhvng.mongodb.net/w
     useUnifiedTopology: true
 })
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -3,133 +3,47 @@ import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
+import api from './services/api';
+
+import DevItem from './components/DevItem/';
+import DevForm from './components/DevForm/';
 //Componente -> Bloco isolado de HTML, CSS, Javascript o qual não interfere no restante da aplicação
 //Propriedade -> Informações que um componente PAI passa para seus componentes filhos
 //Estado -> Informações mantidas pelo componente (Lembrar: Imutabilidade)
 
 function App() {
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [devs, setDevs] = useState([]);
 
-  const [techs, setTechs] = useState('');
-  const [github_username, setGithubUsername] = useState('');
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
 
-  useEffect(() =>{
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { latitude, longitude } = position.coords;
+      setDevs(response.data);
+    }
 
-      setLatitude(latitude);
-      setLongitude(longitude);
-    }, (error) => {
+    loadDevs();
+  }, []);
 
-    }, 
-    {
-      timeout: 30000
-    });
-  })
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadadstrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input 
-              name="github_username" 
-              id="github_username" 
-              required 
-              value={github_username}
-              onChange={(e) => setGithubUsername(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input 
-              name="techs" 
-              id="techs" 
-              required 
-              value={techs}
-              onChange={(e) => setTechs(e.target.value)}
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input 
-                type="number" 
-                name="latitude" 
-                id="latitude" 
-                required 
-                value={latitude} 
-                onChange={(e) => setLatitude(e.target.value)} 
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input 
-                type="number" 
-                name="longitude" 
-                id="longitude" 
-                required 
-                value={longitude} 
-                onChange={(e) => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/42386521?s=460&v=4" alt="Phelipe Soares"/>
-              <div className="user-info">
-                <strong>Phelipe Soares</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Desenvolvedor Jr. Aprendendo tecnlogias</p>
-            <a href="https://github.com/fannarky">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/42386521?s=460&v=4" alt="Phelipe Soares"/>
-              <div className="user-info">
-                <strong>Phelipe Soares</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Desenvolvedor Jr. Aprendendo tecnlogias</p>
-            <a href="https://github.com/fannarky">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/42386521?s=460&v=4" alt="Phelipe Soares"/>
-              <div className="user-info">
-                <strong>Phelipe Soares</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Desenvolvedor Jr. Aprendendo tecnlogias</p>
-            <a href="https://github.com/fannarky">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/42386521?s=460&v=4" alt="Phelipe Soares"/>
-              <div className="user-info">
-                <strong>Phelipe Soares</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Desenvolvedor Jr. Aprendendo tecnlogias</p>
-            <a href="https://github.com/fannarky">Acessar perfil no Github</a>
-          </li>
+          {
+            devs.map(dev => (
+              <DevItem key={dev._id} dev={dev} />
+            ))
+          }
         </ul>
       </main>
     </div>
